e82deda3a9964ba0f0f7df755a9c79642bb3a9af
unknown <ilmsmjettison@gmail.com>
2022-02-05 18:36:51 +0800

    add mobile navbar

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,7 @@
 let hamburger = document.getElementById('hamburger-1');
+let mobNav = document.querySelector('.mobileNav');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
+    mobNav.classList.toggle('showNav');
 })
