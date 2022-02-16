e224028379722f17f670cfaea60bc772a1aa0728
unknown <ilmsmjettison@gmail.com>
2022-02-16 11:20:25 +0800

    save

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,9 @@
 let hamburger = document.getElementById('hamburger-1');
 let mobNav = document.querySelector('.mobileNav');
-let innerCursor = document.querySelector('.inner-cursor');
-let outerCursor = document.querySelector('.outer-cursor');
 
-document.addEventListener('mousemove', moveCursor)
-
-function moveCursor(e) {
-    let x = e.clientX;
-    let y = e.clientY;
-    
-    // console.log(x, y);
-    innerCursor.style.left = `${x}px`;
-    innerCursor.style.top = `${y}px`;
-    outerCursor.style.left = `${x}px`;
-    outerCursor.style.top = `${y}px`;
-}
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
     mobNav.classList.toggle('showNav');
 })
+
--- JS/swiper.js
+++ JS/swiper.js
@@ -15,14 +15,20 @@ const swiper = new Swiper(".swiper", {
     },
 
     // effect
-    effect: "fade",
-    fadeEffect: {
-        crossFade: true,
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            // will set `translateZ(-400px)` on previous slides
+            translate: [0, 0, -400],
+        },
+        next: {
+            // will set `translateX(100%)` on next slides
+            translate: ["100%", 0, 0],
+        },
     },
 
     keyboard: {
         enabled: true,
         onlyInViewport: false,
     },
-
 });
